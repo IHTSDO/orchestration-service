0eca2dd2278748adb3a0d147e8d477438fa96c3b
Kai Kewley <kaikewley@gmail.com>
2015-03-09 21:27:36 +0000

    WRP-138 - select all changes button (also deselect all)

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
 	console.log("Document ready");
 	var conceptRowTemplate = Handlebars.compile($("#concept-template").html());
 
+	$(".select-all").click(function() {
+		var $button = $(this);
+		var $checkboxes = $(":checkbox", $button.parent().parent());
+		if ($button.data("all") != "yes") {
+			$button.data("all", "yes");
+			$checkboxes.not(":checked").click();
+			$button.text("Deselect all");
+		} else {
+			$button.data("all", "no");
+			$checkboxes.filter(":checked").click();
+			$button.text("Select all");
+		}
+	});
+
 	$.ajax({
 		dataType: "json",
 		url: "mock_response/backlog.json",// TODO: Remove mock REST endpoint once UI developed
