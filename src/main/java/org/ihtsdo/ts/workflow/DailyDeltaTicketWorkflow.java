b9403895667b104ace8bc20d868cdabce85eed55
Kai Kewley <kaikewley@gmail.com>
2015-03-17 16:51:03 +0000

    Run classifier via workflow evaluating results.

--- src/main/java/org/ihtsdo/ts/workflow/DailyDeltaTicketWorkflow.java
+++ src/main/java/org/ihtsdo/ts/workflow/DailyDeltaTicketWorkflow.java
@@ -1,8 +1,14 @@
 package org.ihtsdo.ts.workflow;
 
 import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraException;
 import org.apache.commons.lang.NotImplementedException;
+import org.ihtsdo.ts.importer.JiraTransitions;
 import org.ihtsdo.ts.importer.clients.jira.JQLBuilder;
+import org.ihtsdo.ts.importer.clients.jira.JiraProjectSync;
+import org.ihtsdo.ts.importer.clients.snowowl.ClassificationResults;
+import org.ihtsdo.ts.importer.clients.snowowl.SnowOwlRestClient;
+import org.ihtsdo.ts.importer.clients.snowowl.SnowOwlRestClientException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,29 +18,25 @@ import javax.annotation.Resource;
 @Resource
 public class DailyDeltaTicketWorkflow implements TicketWorkflow {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private SnowOwlRestClient snowOwlRestClient;
+
+	@Autowired
+	private JiraProjectSync jiraProjectSync;
 
 	private final String interestingTicketsJQL;
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	public DailyDeltaTicketWorkflow(String jiraProjectKey) {
-		interestingTicketsJQL = new JQLBuilder().project(jiraProjectKey).statusNot(State.PROMOTED.toString()).toString();
-	}
-
-	public static enum State {
-		CREATED,
-		IMPORTED,
-		CLASSIFICATION_QUERIES,
-		CLASSIFICATION_SUCCESS,
-		EXPORTED,
-		BUILT,
-		VALIDATED,
-		ACCEPTED,
-		REJECTED,
-		PROMOTED,
-		PUBLISHED,
-		FAILED,
-		CLOSED
+		interestingTicketsJQL = new JQLBuilder()
+				.project(jiraProjectKey)
+				.statusNot(State.FAILED.toString())
+				.statusNot(State.PROMOTED.toString())
+				.statusNot(State.PUBLISHED.toString())
+				.statusNot(State.CLOSED.toString())
+				.toString();
 	}
 
 	@Override
@@ -44,19 +46,16 @@ public class DailyDeltaTicketWorkflow implements TicketWorkflow {
 
 	@Override
 	public void processChangedTicket(Issue issue) {
-		State currentState = State.valueOf(issue.getStatus().getName().toUpperCase());
+		State currentState = getState(issue);
 		try{
 			switch (currentState) {
-				case CREATED:	doImport();
+				case IMPORTED:	runClassifier(issue);
 								break;
 
-				case IMPORTED:	runClassifier();
-								break;
-
-				case CLASSIFICATION_QUERIES:	logger.info("Ticket {} has classification queries.  Awaiting user action", issue.getKey());
+				case CLASSIFIED_WITH_QUERIES:	logger.info("Ticket {} has classification queries.  Awaiting user action", issue.getKey());
 												break;
 
-				case CLASSIFICATION_SUCCESS:	exportTask();
+				case CLASSIFIED_SUCCESSFULLY:	exportTask();
 												break;
 
 				case EXPORTED:	callSRS();
@@ -86,19 +85,37 @@ public class DailyDeltaTicketWorkflow implements TicketWorkflow {
 			//Attempt to put the ticket into the failed State with a comment to that effect
 			try {
 				issue.addComment(errMsg);
-//				issue.transition().execute(State.FAILED.name()); TODO: Reinstate this once the transition is implemented in Jira
+				issue.transition().execute(JiraTransitions.FAILED);
 			} catch (Exception e2) {
 				logger.error("Additional exception while trying to record previous exception in Jira.", e2);
 			}
 		}
 	}
 
-	private void doImport() {
-		throw new NotImplementedException("Code not yet written to doImport");
+	private State getState(Issue issue) {
+		return State.valueOf(issue.getStatus().getName().toUpperCase().replace(" ", "_"));
 	}
 
-	private void runClassifier() {
-		throw new NotImplementedException("Code not yet written to runClassifier");
+	private void runClassifier(Issue issue) throws SnowOwlRestClientException, InterruptedException, JiraException {
+		ClassificationResults results = snowOwlRestClient.classify(issue.getKey());
+		boolean equivalentConceptsFound = results.isEquivalentConceptsFound();
+		boolean relationshipChangesFound = results.isRelationshipChangesFound();
+		String newStatus;
+		String comment = "";
+		if (!equivalentConceptsFound && !relationshipChangesFound) {
+			newStatus = JiraTransitions.CLASSIFY_WITHOUT_QUERIES;
+			comment = "No issues found by classifier.";
+		} else {
+			newStatus = JiraTransitions.CLASSIFY_WITH_QUERIES;
+			if (equivalentConceptsFound) {
+				comment += "Equivalent concepts found by classifier. ";
+			}
+			if (relationshipChangesFound) {
+				comment += "Redundant stated relationships found by classifier.";
+			}
+		}
+		issue.addComment(comment);
+		jiraProjectSync.updateStatus(issue.getKey(), newStatus);
 	}
 
 	private void versionMain() {
@@ -123,5 +140,21 @@ public class DailyDeltaTicketWorkflow implements TicketWorkflow {
 	
 	private void mergeTaskToMain() {
 		throw new NotImplementedException("Code not yet written to mergeTaskToMain");
+	}
+
+	private static enum State {
+		CREATED,
+		IMPORTED,
+		CLASSIFIED_SUCCESSFULLY,
+		CLASSIFIED_WITH_QUERIES,
+		EXPORTED,
+		BUILT,
+		VALIDATED,
+		ACCEPTED,
+		REJECTED,
+		PROMOTED,
+		PUBLISHED,
+		FAILED,
+		CLOSED
 	}
 }
