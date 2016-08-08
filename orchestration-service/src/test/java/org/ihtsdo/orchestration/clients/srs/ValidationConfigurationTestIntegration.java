4df111a5c226bad64fc7222969c75aebbf49038b
CoderMChu <mch@ihtsdo.org>
2016-08-08 12:49:22 +0100

    Rename ValidationConfigurationIntegrationTest

--- orchestration-service/src/test/java/org/ihtsdo/orchestration/clients/srs/ValidationConfigurationTestIntegration.java
+++ orchestration-service/src/test/java/org/ihtsdo/orchestration/clients/srs/ValidationConfigurationTestIntegration.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {Config.class, TestProperties.class})
-public class ValidationConfigurationIntegrationTest {
+public class ValidationConfigurationTestIntegration {
 	
 	@Autowired ValidationConfiguration defaultConfig;
 	
