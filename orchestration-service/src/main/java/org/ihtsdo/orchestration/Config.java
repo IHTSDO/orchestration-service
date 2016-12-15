24e440495f404ee3eaad6c539a09f43e4d48dd11
Peter Groves Williams <petergwilliams@hotmail.com>
2016-12-15 16:09:44 +0000

    Added Swagger2 support

--- orchestration-service/src/main/java/org/ihtsdo/orchestration/Config.java
+++ orchestration-service/src/main/java/org/ihtsdo/orchestration/Config.java
@@ -1,13 +1,46 @@
 package org.ihtsdo.orchestration;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableSwagger2
 @ImportResource("classpath:spring_context.xml")
 public class Config {
+	
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("default")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build(); 
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("IHTSDO Orchestration - Swagger")
+				.description("IHTSDO Orchestration - Swagger")
+				.contact("techsupport@ihtsdo.org")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://ihtsdo.org")
+				.version("2.0")
+				.build();
+	}
+
 }
